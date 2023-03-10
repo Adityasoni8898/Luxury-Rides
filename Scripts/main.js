dad5e7a6e8914a64e2f5fa236df8fb24d5605fac
Aditya <soniaditya541@gmail.com>
2023-03-10 15:39:42 +0530

    mobile responsiveness 3

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,12 @@
-function myFunction() {
-  var x = document.getElementById("nav_links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+const hamburger = document.querySelector(".hamburger");
+const nav_link_container = document.querySelector(".nav_link_container");
+
+hamburger.addEventListener("click", ()=>{
+  hamburger.classList.toggle("active");
+  nav_link_container.classList.toggle("active");
+})
+
+.document.querySelectorAll(".nav_link_container").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  nav_link_container.classList.remove("active");
+}))
